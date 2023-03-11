68658dce9d86787bb03309d64d68370f249a625a
ChandralingamP <chandralingam2003@gmail.com>
2023-03-11 10:17:21 +0530

    mongourl

--- index.js
+++ index.js
@@ -11,8 +11,9 @@ app.use(cors());
 app.use(express.json());
 
 // let uri = "mongodb://127.0.0.1:27017/MusicApp?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.6.2";
-let uri = "mongodb://musicapp:musicapp@ac-jl3soxk-shard-00-00.bwqynof.mongodb.net:27017,ac-jl3soxk-shard-00-01.bwqynof.mongodb.net:27017,ac-jl3soxk-shard-00-02.bwqynof.mongodb.net:27017/MusicApp?ssl=true&replicaSet=atlas-sjms9a-shard-0&authSource=admin&retryWrites=true";
+// let uri = "mongodb://musicapp:musicapp@ac-dxfsspn-shard-00-00.vyd5gpf.mongodb.net:27017,ac-dxfsspn-shard-00-01.vyd5gpf.mongodb.net:27017,ac-dxfsspn-shard-00-02.vyd5gpf.mongodb.net:27017/MusicApp?ssl=true&replicaSet=atlas-aquhdh-shard-0&authSource=admin&retryWrites=true&w=majority";
 mongoose.connect(process.env.MONGO_URL);
+// mongoose.connect(uri);
 const connection = mongoose.connection;
 connection.once("open", () => {
   console.log("Connection made");
